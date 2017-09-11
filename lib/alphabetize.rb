70ea89d7e67781b8970cef26f394d320e9a174c6
Rebecca Newborn <rjn58@cornell.edu>
2017-09-11 02:05:08 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,12 @@
 def alphabetize(arr)
   # code here
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  result = arr.sort_by do |element|
+    array = []
+    for i in 0..element.length-1
+      array << alphabet.index(element[i])
+    end
+    array
+  end
+  result
 end
